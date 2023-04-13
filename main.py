4242ffca7661fb90cbce3dd5a8f299501d47061f
CHE <worasornjunpuan@gmail.com>
2023-04-13 13:00:02 +0700

    first commit for main.py
    finish the v1.0
    - loop for input
    - create dir for collect the mp3 file

---
+++ main.py
@@ -0,0 +1,42 @@
+# import the library
+from pytube import YouTube  # pip3 install pytube
+import os
+
+# loop for user to input the URL
+user_in = True
+list_url = []  # for collect the URL
+url_count = 1
+
+print("Enter the URL of the YouTube video you want to download :")
+
+while user_in == True:
+    url = str(input(f" URL {url_count} :"))
+    url_count += 1
+    if url != "":
+        list_url.append(url)
+        print(list_url)
+    elif url == "":
+        user_in = False
+print()
+print(f"preparing {len(list_url)} items")
+
+# create a dir
+try:
+    os.mkdir("mp3")
+    print()
+except FileExistsError:
+    print()
+
+# loop for download and save the file
+for i in list_url:
+    yt = YouTube(i)
+    print("downloading")
+    video = yt.streams.filter(only_audio=True).first()
+    out_file = video.download("./mp3")
+
+    # save the file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+    print(f"finish {list_url.index(i)+ 1} /{len(list_url)}")
+    print()
